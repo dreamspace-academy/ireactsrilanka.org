73b268f35ef4f03c538b0d2f6010f2f3e27f3077
GunaRakulan-GR <gunarakulan@gmail.com>
2021-10-07 03:56:46 +0530

    Test3

--- file-creator.php
+++ file-creator.php
@@ -2,9 +2,7 @@
 
 
 $save_status = file_put_contents("file.txt", "Hello"); //generate json file
-
-$myfile1 = fopen("file.txtt", "r") or die("Unable to open file!");
-echo fread($myfile1,filesize("file.txt"));
+echo $save_status;
 
 
 ?>
